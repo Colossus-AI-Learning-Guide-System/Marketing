e0938e009bc5d9408c9aa1a350904712191a36d4
Tharana Navithaka Bopearachchi <tharanabope30@gmail.com>
2025-02-25 14:42:06 +0530

    Remove static export configuration in Next.js config

--- fullstack/next.config.js
+++ fullstack/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enable static export
   images: {
     domains: ["images.unsplash.com", "i.imgur.com"],
-    unoptimized: true, // Disable image optimization for static export
   },
 };
 
